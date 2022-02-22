3bc897a89281ada1975b54e729693aa07c96af40
hdy <hdy0506@gmail.com>
2022-02-23 08:39:40 +0900

    interesting digits

---
+++ InterestingDigits.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class InterestingDigits {
+    public static int[] digits(int base){
+        ArrayList<Integer> array = new ArrayList();
+        for(int i=2;i<base;i++){
+            if((base-1) % i == 0){
+                array.add(i);
+            }
+        }
+
+        int[] result = array.stream().mapToInt(i -> i).toArray();
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(digits(10)));
+    }
+}
