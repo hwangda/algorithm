f7e23cb2c8598a748ff402f8e37257c124a7cc16
hdy <hdy0506@gmail.com>
2022-02-24 11:04:53 +0900

    불필요 변수 제거

--- ThePalindrome.java
+++ ThePalindrome.java
@@ -1,21 +1,18 @@
 public class ThePalindrome {
     public static int find(String s){
-        int start = 0;
         int end = s.length() - 1;
         String addString = "";
 
         for(int i=0;i<s.length();i++){
-            if(start == end){
+            if(i == end){
                 break;
             }
             // System.out.println(start + ", " + s.charAt(start));
             // System.out.println(end + ", " + s.charAt(end));
             // 양쪽 끝에서부터 비교 / 데칼코마니처럼
-            if(s.charAt(start) != s.charAt(end)){ // start , end 가 다르면 start +=1
+            if(s.charAt(i) != s.charAt(end)){ // start , end 가 다르면 start +=1
                 addString = s.charAt(i) + addString;
-                start += 1;
             }else{
-                start += 1;
                 end -= 1;
             }
             // System.out.println(addString);
